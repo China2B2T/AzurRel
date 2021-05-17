6f05830cac5bf67085abd4f01afa876cfafb1549
401395069 <401395069@qq.com>
2021-05-17 18:15:43 +0800

    prepare for prior queue

--- src/main/java/org/china2b2t/azurmgr/Main.java
+++ src/main/java/org/china2b2t/azurmgr/Main.java
@@ -5,10 +5,12 @@ import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.china2b2t.azurmgr.command.CommandGenkey;
 import org.china2b2t.azurmgr.command.CommandPluginmgr;
 import org.china2b2t.azurmgr.http.Server;
 import org.china2b2t.azurmgr.listener.PlayerListener;
+import org.china2b2t.azurmgr.listener.PluginMessage;
 
 // LyoqCiAqIEkgV0lMTCBBTFdBWVMgVE8gQkxBTUUgTVlTRUxGCiAqIFdIWSBBTSBJIFNPIFNFTEZJU0gKICogV0hZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZCiAqLw==
 
@@ -33,7 +35,7 @@ public class Main extends JavaPlugin {
             Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         }
         // Should I get current list from BungeeCord?
-        // getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new Channel());
+        getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new PluginMessage());
         saveDefaultConfig();
         int apiPort = this.getConfig().getInt("port");
         boolean isApiEnabled = Server.startServer(apiPort);
---
+++ src/main/java/org/china2b2t/azurmgr/listener/PluginMessage.java
@@ -0,0 +1,12 @@
+package org.china2b2t.azurmgr.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+public class PluginMessage implements PluginMessageListener {
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        
+    }
+    
+}
